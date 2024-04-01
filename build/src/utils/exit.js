39d443d9740eaabb13af2ea3d1ee67dd6615e673
Vítor O. <Caulicons@hotmail.com>
2024-04-01 14:27:26 -0300

    (fix🧹): remove build folder

--- build/src/utils/exit.js
+++
@@ -1,8 +0,0 @@
-export default function exit() {
-    console.log("\n🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳\n");
-    console.log("Projeto Desenvolvido por: Vítor Oliveira.\n");
-    console.log("LinkedIn 🔗 - https://www.linkedin.com/in/vitor-oliveira-7aa003238/");
-    console.log("GitHub   🐈 - https://github.com/Caulicons");
-    console.log("\n🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳🌳");
-    process.exit(0);
-}
--- build/src/utils/sleep.js
+++
@@ -1,5 +0,0 @@
-var sleep = function (ms) {
-    if (ms === void 0) { ms = 2000; }
-    return new Promise(function (resolve) { return setTimeout(resolve, ms); });
-};
-export default sleep;
